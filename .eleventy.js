afba490f2af3c3e9fcbd8a2247a62bba6bc944e3
Nathan Smith <nathan@nathansmith.io>
2020-06-12 23:17:13 -0700

    Add a lot of styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
 const yaml = require('js-yaml')
 const katex = require('katex')
+const pluginSass = require('eleventy-plugin-sass')
 
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(pluginSass, { watch: 'source/*.{scss,sass}' })
+
   eleventyConfig.addFilter('katex', (value) => {
     if (value.toLowerCase() === 'latex') {
-      return katex.renderToString('\\LaTeX')
+      return katex.renderToString('\\LaTeX').trim()
     }
-    return value
+    return value.trim()
   })
+
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.safeLoad(contents))
   // eleventyConfig.dir = { data: '.' }
   // return eleventyConfig
