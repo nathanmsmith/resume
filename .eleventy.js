33df55d3e023b71f7e6f70a7b2dd6d8b9376dc54
Nathan Smith <nathan@nathansmith.io>
2020-06-12 19:42:59 -0700

    Add katex rendering

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 const yaml = require('js-yaml')
+const katex = require('katex')
 
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addFilter('katex', (value) => {
+    if (value.toLowerCase() === 'latex') {
+      return katex.renderToString('\\LaTeX')
+    }
+    return value
+  })
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.safeLoad(contents))
   // eleventyConfig.dir = { data: '.' }
   // return eleventyConfig
